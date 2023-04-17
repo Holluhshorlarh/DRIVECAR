4a17011c44a6e6a090e1e6c82c477b41ce3a1ce5
Holluhshorlarh <olusholaajayi1400@gmail.com>
2023-04-18 00:06:11 +0100

    user schema implementation

--- models/user.js
+++ models/user.js
@@ -1,4 +1,38 @@
-const mongoose = require("mongoose")
-const userSchema = new mongoose.Schema(
-
-)
+const validateIdNumber = (value) => {
+    const idNumberRegex = /^[0-9]{12}$/;
+    return idNumberRegex.test(value);
+  };
+  
+  const userSchema = new mongoose.Schema(
+    {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+        default: null,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      idType: {
+        type: String,
+        enum: ["international passport", "drivers license", "voters card"],
+        required: true,
+      },
+      idNumber: {
+        type: String,
+        required: true,
+        validate: [validateIdNumber, "Invalid ID number"],
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  const User = mongoose.model("User", userSchema);
+  
+  module.exports = User;  
