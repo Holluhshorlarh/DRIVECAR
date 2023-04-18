73dd978db7e335364e3feea7162849c7da279e66
Holluhshorlarh <olusholaajayi1400@gmail.com>
2023-04-18 13:02:30 +0100

    user model updated

--- models/user.js
+++ models/user.js
@@ -1,20 +1,26 @@
-const validateIdNumber = (value) => {
-    const idNumberRegex = /^[0-9]{12}$/;
-    return idNumberRegex.test(value);
-  };
-  
-  const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-  const userSchema = new mongoose.Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    idNumber: {type: String,enum: ['International Passport', 'Drivers license', 'Voters card'], 
-    required: true, validate: [validateIdNumber, "Invalid ID number"]},
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
-  });
-  
-  module.exports = mongoose.model('User', userSchema);
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  idNumber: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^[A-Za-z0-9]{0,12}$/.test(v);
+      },
+      message: props =>
+        `${props.value} is not a valid ID number. It should be alphanumeric and have a maximum length of 12 characters.`,
+    },
+  },
+  isActive: { type: Boolean, default: true },
+}, { timestamps: true });
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
