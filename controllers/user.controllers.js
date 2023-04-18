42987524d193889d44bccfc5ce48a380c2ce90e6
Holluhshorlarh <olusholaajayi1400@gmail.com>
2023-04-18 14:20:51 +0100

    User controller revamped

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -23,11 +23,12 @@ exports.userSignup = async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
     // Create new user object and save to database
+    const { firstName, lastName, age, email } = req.body;
     const user = new User({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      age: req.body.age,
-      email: req.body.email,
+      firstName,
+      lastName,
+      age,
+      email,
       password: hashedPassword,
     });
     await user.save();
@@ -35,7 +36,7 @@ exports.userSignup = async (req, res) => {
     // Create JWT token for the user
     const token = jwt.sign({ userId: user._id }, JWT_SECRET);
 
-    return res.status(201).json({ token });
+    return res.status(201).json({ token, message: `${firstName} account created successfully!` });
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({ error: 'Internal server error' });
@@ -65,6 +66,31 @@ exports.userLogin = async (req, res) => {
     const token = jwt.sign({ userId: user._id }, JWT_SECRET);
 
     return res.status(200).json({ token });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Soft delete user by setting isActive property to false
+exports.softDeleteUser = async (req, res) => {
+  try {
+    // Check if authenticated user is admin
+    if (!req.user.isAdmin) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    // Check if user with given id exists
+    const userToDelete = await User.findById(req.params.userId);
+    if (!userToDelete) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Soft delete user by setting isActive property to false
+    userToDelete.isActive = false;
+    await userToDelete.save();
+
+    return res.status(200).json({ message: 'User deleted' });
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({ error: 'Internal server error' });
